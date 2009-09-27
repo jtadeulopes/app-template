c4929acc39fdc9dc126b9a5f5e2b2edcb55be179
Jésus Lopes <jtadeulopes@gmail.com>
2009-09-27 15:37:24 -0300

    Refatoração no arquivo de template.

--- app-template.rb
+++ app-template.rb
@@ -17,19 +17,13 @@ TXT
 run "cp config/database.yml config/database.yml.sample"
 run "cp config/database.yml config/database.yml.production"
  
-# Rspec
-if USE_RSPEC = yes?("Do you want to use RSpec for testing? (yes or no)")
-  append_file TEST_FILE, %(\n\nconfig.gem "rspec", :lib => false)
-  append_file TEST_FILE, %(\nconfig.gem "rspec-rails", :lib => false)
-end
- 
 # remove arquivos desnecessários
 run "rm public/index.html"
 run "rm public/favicon.ico"
 run "rm README"
 run "rm doc/README_FOR_APP"
  
-# baixa arquivo de locale pt-BR
+# I18n locale pt-BR
 file "config/locales/pt-BR.yml", open("http://github.com/jtadeulopes/app-template/raw/master/locale/pt-BR.yml").read
  
 # define a rota default da app
@@ -38,21 +32,32 @@ route %(map.root :controller => "home")
 # gera um controller home
 generate :controller, "home", "index"
  
-# instala plugins
+#
+# Plugins
+#
 if USE_PAPERCLIP = yes?("Install plugin paperclip? (yes or no)")
   plugin "paperclip", :git => "git://github.com/thoughtbot/paperclip.git"
 end
-plugin "jrails", :git => "git://github.com/aaronchi/jrails.git" if yes?("Install plugin jrails? (yes or no)")
+plugin "jrails", :git => "git://github.com/aaronchi/jrails.git"                                     if yes?("Install plugin jrails? (yes or no)")
 plugin "restful_authentication", :git => "git://github.com/technoweenie/restful-authentication.git" if yes?("Install plugin restful-authentication? (yes or no)")
-plugin "nifty-generators", :git => "git://github.com/jtadeulopes/nifty-generators.git" if yes?("Install plugin nifty-generators? (yes or no)")
-plugin "booleanize", :git => "git://github.com/cassiomarques/booleanize.git" if yes?("Install plugin booleanize? (yes or no)")
-plugin "i18n_label", :git => "git://github.com/iain/i18n_label.git" if yes?("Install plugin i18n_label? (yes or no)")
+plugin "nifty-generators", :git => "git://github.com/jtadeulopes/nifty-generators.git"              if yes?("Install plugin nifty-generators? (yes or no)")
+plugin "booleanize", :git => "git://github.com/cassiomarques/booleanize.git"                        if yes?("Install plugin booleanize? (yes or no)")
+plugin "i18n_label", :git => "git://github.com/iain/i18n_label.git"                                 if yes?("Install plugin i18n_label? (yes or no)")
  
-# define o locale default e o time zone
+# config/environment.rb
 environment %(config.i18n.default_locale = "pt-BR")
 environment %(config.time_zone = "Brasilia")
  
-# rspec
+#
+# Test
+#
+ 
+# Rspec
+if USE_RSPEC = yes?("Do you want to use RSpec for testing? (yes or no)")
+  append_file TEST_FILE, %(\n\nconfig.gem "rspec", :lib => false)
+  append_file TEST_FILE, %(\nconfig.gem "rspec-rails", :lib => false)
+end
+
 if USE_RSPEC
 
   # gera esqueleto do rspec
@@ -60,33 +65,41 @@ if USE_RSPEC
 
   # remarkable
   if yes?("Use Remarkable? (yes or no)")
-    # baixa arquivo locale pt-BR para o remarkable
-    file "config/locales/pt-BR-remarkable.yml", open("http://github.com/jtadeulopes/app-template/raw/master/locale/pt-BR-remarkable.yml").read
-    # configurações para o remarkable 
+
+    # configurações
     append_file TEST_FILE, %(\nconfig.gem "remarkable_rails", :lib => false)
     gsub_file 'spec/spec_helper.rb', /(require 'spec\/rails'.*)/, "\\1\nrequire 'remarkable_rails'"
-    # remarkable plugin para testar upload com paperclip
+
+    # locale pt-BR
+    file "config/locales/pt-BR-remarkable.yml", open("http://github.com/jtadeulopes/app-template/raw/master/locale/pt-BR-remarkable.yml").read
+
+    # plugins
     plugin "remarkable_paperclip", :git => "git://github.com/dcrec1/remarkable_paperclip.git" if USE_PAPERCLIP
+    plugin "remarkable_extensions", :git => "git://github.com/dcrec1/remarkable_extensions.git"
+    plugin "remarkable_authlogic", :git => "git://github.com/daviscabral/remarkable_authlogic.git"
   end
 
-  # Machinist
+  # machinist
   if yes?("Use Machinist? (yes or no)")
+
     # configurações
     append_file TEST_FILE, %(\nconfig.gem 'notahat-machinist', :lib => 'machinist', :source => 'http://gems.github.com')
     gsub_file 'spec/spec_helper.rb', /(require 'remarkable_rails'.*)/, "\\1\nrequire File.join(File.dirname(__FILE__), 'blueprints')"
+
     # cria arquivo blueprint
     file "spec/blueprints.rb", <<-TXT
 require 'machinist/active_record'
 # blueprints
     TXT
+
   end
 
 end 
 
-# gems
-
-# Authlogic
-if yes?("Use Authlogic for authentication?") 
+#
+# Gems
+#
+if yes?("Use Authlogic for authentication? (yes or no)") 
   gem "authlogic" 
   file "config/locales/pt-BR-authlogic.yml", open("http://github.com/jtadeulopes/app-template/raw/master/locale/pt-BR-authlogic.yml").read
 end
@@ -94,22 +107,21 @@ if yes?("Install gem Formtastic? (yes or no)")
   gem "justinfrench-formtastic", :lib => 'formtastic', :source  => 'http://gems.github.com' 
   file "config/locales/pt-BR-formtastic.yml", open("http://github.com/jtadeulopes/app-template/raw/master/locale/pt-BR-formtastic.yml").read
 end
-gem "searchlogic" if yes?(" Install gem Searchlogic? (yes or no)")
-gem "brazilian-rails", :version => "2.1.8" if yes?(" Install gem brazilian-rails? (yes or no)")
-gem "rack", :version => "1.0.0" if yes?("Install gem rack? (yes or no) - Require for Locaweb")
+gem "searchlogic"                           if yes?(" Install gem Searchlogic? (yes or no)")
+gem "brazilian-rails", :version => "2.1.8"  if yes?(" Install gem brazilian-rails? (yes or no)")
+gem "rack", :version => "1.0.0"             if yes?("Install gem rack? (yes or no) - Require for Locaweb")
 
 rake "gems:install", :sudo => true
 rake "gems:install", :env => "test", :sudo => true
 
-# capistrano
+#
+# Capistrano
+#
 if yes?("Should I run capify? (yes or no)")
   capify! 
   run "rm config/deploy.rb"
   file "config/deploy.rb", open("http://github.com/jtadeulopes/app-template/raw/master/deploy/deploy.rb").read
 end
  
-# freeze rails edge
-freeze! if yes?("Should I freeze Rails Edge? (yes or no)")
-
 # ignore password logging
 gsub_file "app/controllers/application_controller.rb", /# *(filter_parameter_logging :password)/sm, '\1'
